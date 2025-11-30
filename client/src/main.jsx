c4c4db68ba3b8d3fed95072f7a24293eab41cbe6
tako <dorian2992@hotmail.fr>
2025-11-30 12:04:50 -0500

    Switch to HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import './i18n'
@@ -11,9 +11,9 @@ console.log('Main.jsx is running!');
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ErrorBoundary>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </ErrorBoundary>
     </React.StrictMode>,
 )
